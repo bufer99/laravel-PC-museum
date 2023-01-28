9ce49371dd451520b3e0efa6330c15728b18da16
bufer99 <bufer99@gmail.com>
2023-01-28 17:22:07 +0100

    https pagination bugfix

--- bead2/app/Http/Controllers/ItemController.php
+++ bead2/app/Http/Controllers/ItemController.php
@@ -25,10 +25,13 @@ class ItemController extends Controller
 
     public function labels(Label $label)
     {
+        $items = Label::find($label->id)->item()->paginate(5);
+        $items->setPath('/');
+
         error_log('LABEL: ' . $label);
         return Inertia::render('Home', [
             'label' => $label,
-            'items' => Label::find($label->id)->item()->paginate(5),
+            $items,
             'items_count' => Label::find($label->id)->item()->count(),
         ]);
     }
